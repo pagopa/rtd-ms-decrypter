4d66907e3800846406b3430372ab8944f2dbed82
Giovanni <g.mancini.0@gmail.com>
2022-02-10 17:35:15 +0100

    Add Basic Spring Boot Application

---
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsdecrypter/RtdMsDecrypterApplication.java
@@ -0,0 +1,13 @@
+package it.gov.pagopa.rtd.ms.rtdmsdecrypter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtdMsDecrypterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtdMsDecrypterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/gov/pagopa/rtd/ms/rtdmsdecrypter/RtdMsDecrypterApplicationTests.java
@@ -0,0 +1,13 @@
+package it.gov.pagopa.rtd.ms.rtdmsdecrypter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtdMsDecrypterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
