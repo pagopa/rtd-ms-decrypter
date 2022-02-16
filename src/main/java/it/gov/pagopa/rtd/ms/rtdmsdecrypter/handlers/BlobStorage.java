87ebb9eca5874cf73d23065ed496f9f3181aa5c6
Giovanni <g.mancini.0@gmail.com>
2022-02-16 23:12:52 +0100

    [CEN-1360] Add payload type, model and functional logic to parse the event

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmsdecrypter/handlers/BlobStorage.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsdecrypter/handlers/BlobStorage.java
@@ -4,13 +4,15 @@ import java.util.function.Consumer;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import lombok.extern.slf4j.Slf4j;
+
 
 @Configuration
-public
-class BlobStorage {
+@Slf4j
+public class BlobStorage {
   
-  @Bean
-  public Consumer<String> decrypt() {
-    return message -> System.out.println("Received message " + message);
-  }
+  // @Bean
+  // public Consumer<String> decrypt() {
+  //   return log::info;
+  // }
 }
