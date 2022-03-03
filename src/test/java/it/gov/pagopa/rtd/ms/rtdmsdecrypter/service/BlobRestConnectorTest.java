0dbd6cfd892af50d5fe6f42348c8ccb78ed68563
Giovanni <g.mancini.0@gmail.com>
2022-03-03 17:33:42 +0100

    [CEN-1363] Move some blobconnector tests in model

--- src/test/java/it/gov/pagopa/rtd/ms/rtdmsdecrypter/service/BlobRestConnectorTest.java
+++ src/test/java/it/gov/pagopa/rtd/ms/rtdmsdecrypter/service/BlobRestConnectorTest.java
@@ -1,12 +1,56 @@
 package it.gov.pagopa.rtd.ms.rtdmsdecrypter.service;
 
-import org.junit.jupiter.api.Test;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.context.ActiveProfiles;
+
+import it.gov.pagopa.rtd.ms.rtdmsdecrypter.model.BlobApplicationAware;
+
+
+@SpringBootTest
+@ActiveProfiles("test")
+@EmbeddedKafka(topics = {
+  "rtd-platform-events" }, partitions = 1, bootstrapServersProperty = "spring.cloud.stream.kafka.binder.brokers")
 public class BlobRestConnectorTest {
 
-  @Test
-  void shouldGetRtdSasToken() {
+  @Autowired
+  BlobRestConnector blobRestConnector;
+ 
 
+  @Test
+  void shouldGetSasToken() {
+    // SasResponse myResponse = new SasResponse();
+    // myResponse.setSas("sasToken");
+    // myResponse.setAuthorizedContainer("container");
+    // // when(client.postRtdSas(anyString(), anyString())).thenReturn(myResponse);
+    // when(client.postAdeSas(anyString(), anyString())).thenReturn(myResponse);
+
+    // blobRestConnector.postSas(IngestionApplication.RTD);
+
+    // verify(client, times(1)).postRtdSas(anyString(), anyString());
+
+    // blobRestConnector.postSas(IngestionApplication.ADE);
+
+    // verify(client, times(1)).postAdeSas(anyString(), anyString());
+  }
+
+  @Test
+  void shouldGet() {
+    String container = "rtd-transactions-32489876908u74bh781e2db57k098c5ad034341i8u7y";
+    String blobName = "CSTAR.99910.TRNLOG.20220228.103107.001.csv.pgp";
+    BlobApplicationAware blob = new BlobApplicationAware("/blobServices/default/containers/" + container + "/blobs/" + blobName);
+
+    // blobRestConnector.get(blob);
+   
   }
   
 }
