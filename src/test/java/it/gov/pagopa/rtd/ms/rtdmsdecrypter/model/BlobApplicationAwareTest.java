4d0da7a5307ab1fbe42ba73931dec106ba38ea6e
Giovanni <g.mancini.0@gmail.com>
2022-03-03 21:17:49 +0100

    [CEN-1363] Improve coverage

--- src/test/java/it/gov/pagopa/rtd/ms/rtdmsdecrypter/model/BlobApplicationAwareTest.java
+++ src/test/java/it/gov/pagopa/rtd/ms/rtdmsdecrypter/model/BlobApplicationAwareTest.java
@@ -6,18 +6,36 @@ import org.junit.jupiter.api.Test;
 
 class BlobApplicationAwareTest {
   @Test
-  void shouldMatchRegex() {
+  void shouldMatchRegexRTD() {
     String container = "rtd-transactions-32489876908u74bh781e2db57k098c5ad034341i8u7y";
     String blob = "CSTAR.99910.TRNLOG.20220228.103107.001.csv.pgp";
     String blobUri = "/blobServices/default/containers/" + container + "/blobs/" + blob;
     BlobApplicationAware myBlob = new BlobApplicationAware(blobUri);
-    assertSame(BlobApplicationAware.Application.RTD, myBlob.getApp() );
+    assertSame(BlobApplicationAware.Application.RTD, myBlob.getApp());
   }
-
+  
   @Test
-  void shouldNotMatchRegex() {
+  void shouldMatchRegexADE() {
+    String container = "ade-transactions-xxxxxxxxxx8u74bh781e2db57k098c5ad034341i8u7y";
+    String blob = "ADE.45678.TRNLOG.20220228.103107.001.csv.pgp";
+    String blobUri = "/blobServices/default/containers/" + container + "/blobs/" + blob;
+    BlobApplicationAware myBlob = new BlobApplicationAware(blobUri);
+    assertSame(BlobApplicationAware.Application.ADE, myBlob.getApp());
+  }
+  
+  @Test
+  void shouldMatchRegexNoApp() {
     String container = "rtd-loremipsum-32489876908u74bh781e2db57k098c5ad034341i8u7y";
     String blob = "CSTAR.99910.TRNLOG.20220228.103107.001.csv.pgp";
+    String blobUri = "/blobServices/default/containers/" + container + "/blobs/" + blob;
+    BlobApplicationAware myBlob = new BlobApplicationAware(blobUri);
+    assertSame(BlobApplicationAware.Application.NOAPP, myBlob.getApp());
+  }
+  
+  @Test
+  void shouldNotMatchRegexNo() {
+    String container = "myContainer";
+    String blob = "myBlob";
     String blobUri = "/blobServices/default/containers/" + container + "/blobs/" + blob;
     BlobApplicationAware myBlob = new BlobApplicationAware(blobUri);
     assertSame(BlobApplicationAware.Application.NOAPP, myBlob.getApp() );
