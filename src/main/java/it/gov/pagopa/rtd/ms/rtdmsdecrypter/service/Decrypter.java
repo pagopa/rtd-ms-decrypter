56f9135a4bc442f7532e9d8140e41d9a752f5a92
Giovanni <g.mancini.0@gmail.com>
2022-03-07 12:05:08 +0100

    [CEN-1363] Modify logs for tests

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmsdecrypter/service/Decrypter.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsdecrypter/service/Decrypter.java
@@ -58,7 +58,7 @@ public class Decrypter implements IDecrypter {
 
   @PostConstruct
   private void readKey() throws IOException {
-    log.warn("Private Key: {}", Files.readString(Path.of(this.privateKeyPath)).getBytes(StandardCharsets.UTF_8));
+    log.warn("Private Key: {}", Files.readString(Path.of(this.privateKeyPath)));
     this.privateKey = new String(Base64.getDecoder().decode(Files.readString(Path.of(this.privateKeyPath)).getBytes(StandardCharsets.UTF_8)));
   }
 
