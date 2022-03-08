392418eee2e291d3ed7bf383fbcc4f1b866203e2
Alberto Re <alberto.re@pagopa.it>
2022-03-08 14:47:52 +0100

    [CEN-1434] Create HTTP client with TrustSelfSignedStrategy

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmsdecrypter/configuration/ThreadSafeHttpClient.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsdecrypter/configuration/ThreadSafeHttpClient.java
@@ -1,17 +1,27 @@
 package it.gov.pagopa.rtd.ms.rtdmsdecrypter.configuration;
 
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.ssl.SSLContextBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 
 @Configuration
 public class ThreadSafeHttpClient {
 
   @Bean
-  CloseableHttpClient myHttpClient() {
-    return HttpClients.custom().setConnectionManager(new PoolingHttpClientConnectionManager()).build();
+  CloseableHttpClient myHttpClient() throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
+    SSLContextBuilder builder = new SSLContextBuilder();
+    builder.loadTrustMaterial(null, new TrustSelfSignedStrategy());
+    SSLConnectionSocketFactory sslSocketFactory = new SSLConnectionSocketFactory(builder.build());
+    return HttpClients.custom().setSSLSocketFactory(sslSocketFactory).setConnectionManager(new PoolingHttpClientConnectionManager()).build();
   }
-  
+
 }
