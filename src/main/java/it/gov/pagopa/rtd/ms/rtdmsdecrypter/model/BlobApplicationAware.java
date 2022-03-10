652b06a41e26e6e7db4daa450c38ca98beb92813
tommasolencioni <1Seedeadpeopl>
2022-03-10 12:58:10 +0100

    [CEN-1423] add regex pattern for blob name.

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmsdecrypter/model/BlobApplicationAware.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsdecrypter/model/BlobApplicationAware.java
@@ -39,7 +39,27 @@ public class BlobApplicationAware {
 
   private String targetDir = "/tmp";
 
-  private Pattern uriPattern = Pattern.compile("^.*containers/((ade|rtd)-transactions-[a-z0-9]{44})/blobs/((ADE|CSTAR).*)");
+  //Note: This pattern does not support leap years
+  //TODO Maybe a check for the presence of a real ABI code should be performed
+  //The following pattern matches PagoPA file name's standard
+  //  Specifics can be found at: https://docs.pagopa.it/digital-transaction-register/v/digital-transaction-filter/acquirer-integration-with-pagopa-centrostella/integration/standard-pagopa-file-transactions
+  // It is separated in lines to be easily read and maintained:
+  //  URI (of the blob)
+  //  APP. (ADE or CSTAR)
+  //  ABI.
+  //  TRNLOG. (fixed)
+  //  YYYYMMDD.
+  //  HHMISS.
+  //  nnn.  (max 999 transaction with the same previous fields)
+  //  .*  (matches everything e.g. file extension)
+  private Pattern uriPattern = Pattern.compile("^.*containers/((ade|rtd)-transactions-[a-z0-9]{44})/blobs/" +
+          "((ADE|CSTAR)\\." +
+          "([0-9]{5})\\." +
+          "(TRNLOG)\\." +
+          "((?:(?:(?:(?:(?:[13579][26]|[2468][048])00)|(?:[0-9]{2}(?:(?:[13579][26])|(?:[2468][048]|0[48]))))(?:(?:(?:09|04|06|11)(?:0[1-9]|1[0-9]|2[0-9]|30))|(?:(?:01|03|05|07|08|10|12)(?:0[1-9]|1[0-9]|2[0-9]|3[01]))|(?:02(?:0[1-9]|1[0-9]|2[0-9]))))|(?:[0-9]{4}(?:(?:(?:09|04|06|11)(?:0[1-9]|1[0-9]|2[0-9]|30))|(?:(?:01|03|05|07|08|10|12)(?:0[1-9]|1[0-9]|2[0-9]|3[01]))|(?:02(?:[01][0-9]|2[0-8]))))))\\." +
+          "((?:0[0-9]|1[0-9]|2[0-3])(?:[0-5][0-9]){2})\\." +
+          "([0-9]{3})\\." +
+          ".*)");
 
 
   public BlobApplicationAware(String uri) {
