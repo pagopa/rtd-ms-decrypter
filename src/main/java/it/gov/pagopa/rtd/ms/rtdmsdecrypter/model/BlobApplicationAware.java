754ffeaba59aff7a0d5d559e18cc7335e357ac70
tommasolencioni <1Seedeadpeopl>
2022-03-10 16:07:33 +0100

    [CEN-1423] refactored regex matching for blob name

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmsdecrypter/model/BlobApplicationAware.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsdecrypter/model/BlobApplicationAware.java
@@ -1,5 +1,7 @@
 package it.gov.pagopa.rtd.ms.rtdmsdecrypter.model;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -39,52 +41,75 @@ public class BlobApplicationAware {
 
   private String targetDir = "/tmp";
 
-  //Note: This pattern does not support leap years
-  //The following pattern matches PagoPA file name's standard
-  //  Specifics can be found at: https://docs.pagopa.it/digital-transaction-register/v/digital-transaction-filter/acquirer-integration-with-pagopa-centrostella/integration/standard-pagopa-file-transactions
-  // It is separated in lines to be easily read and maintained:
-  //  URI (of the blob)
-  //  APP. (ADE or CSTAR)
-  //  ABI.
-  //  TRNLOG. (fixed)
-  //  YYYYMMDD.
-  //  HHMISS.
-  //  nnn.  (max 999 transaction with the same previous fields)
-  //  .*  (matches everything e.g. file extension)
-  private Pattern uriPattern = Pattern.compile("^.*containers/((ade|rtd)-transactions-[a-z0-9]{44})/blobs/" +
-          "((ADE|CSTAR)\\." +
-          "([0-9]{5})\\." +
-          "(TRNLOG)\\." +
-          "((?:(?:(?:(?:(?:[13579][26]|[2468][048])00)|(?:[0-9]{2}(?:(?:[13579][26])|(?:[2468][048]|0[48]))))(?:(?:(?:09|04|06|11)(?:0[1-9]|1[0-9]|2[0-9]|30))|(?:(?:01|03|05|07|08|10|12)(?:0[1-9]|1[0-9]|2[0-9]|3[01]))|(?:02(?:0[1-9]|1[0-9]|2[0-9]))))|(?:[0-9]{4}(?:(?:(?:09|04|06|11)(?:0[1-9]|1[0-9]|2[0-9]|30))|(?:(?:01|03|05|07|08|10|12)(?:0[1-9]|1[0-9]|2[0-9]|3[01]))|(?:02(?:[01][0-9]|2[0-8]))))))\\." +
-          "((?:0[0-9]|1[0-9]|2[0-3])(?:[0-5][0-9]){2})\\." +
-          "([0-9]{3})\\." +
-          ".*)");
-
+  private Pattern uriPattern = Pattern.compile("^.*containers/((ade|rtd)-transactions-[a-z0-9]{44})/blobs/((ADE|CSTAR)(\\.)(.*))");
 
   public BlobApplicationAware(String uri) {
     blobUri = uri;
     status = Status.INIT;
 
     Matcher matcher = uriPattern.matcher(uri);
+
     if (matcher.matches()) {
 
       status = Status.RECEIVED;
       container = matcher.group(1);
       blob = matcher.group(3);
 
-      if (matcher.group(2).equalsIgnoreCase("ADE") && matcher.group(4).equalsIgnoreCase("ADE")) {
-        app = Application.ADE;
-        targetContainer = targetContainerAde;
-      } else if (matcher.group(2).equalsIgnoreCase("RTD") && matcher.group(4).equalsIgnoreCase("CSTAR")) {
-        app = Application.RTD;
-        targetContainer = targetContainerRtd;
-      } else {
+      if(checkNameFormat(matcher.group(6).split("\\."))) {
+
+        if (matcher.group(2).equalsIgnoreCase("ADE") && matcher.group(4).equalsIgnoreCase("ADE")) {
+          app = Application.ADE;
+          targetContainer = targetContainerAde;
+        } else if (matcher.group(2).equalsIgnoreCase("RTD") && matcher.group(4).equalsIgnoreCase("CSTAR")) {
+          app = Application.RTD;
+          targetContainer = targetContainerRtd;
+        } else {
+          app = Application.NOAPP;
+        }
+      }
+      else {
         app = Application.NOAPP;
       }
     }
     else {
       app = Application.NOAPP;
     }
+  }
+
+  /**
+   * This method matches PagoPA file name's standard
+   *  Specifics can be found at: https://docs.pagopa.it/digital-transaction-register/v/digital-transaction-filter/acquirer-integration-with-pagopa-centrostella/integration/standard-pagopa-file-transactions
+   * @param uriTokens: values obtained from the name of the blob (separated by dots)
+   * @return true if the name matches the format, false otherwise
+   */
+  private boolean checkNameFormat(String[] uriTokens){
+    //Check for sender ABI code
+    if(uriTokens[0]==null || !uriTokens[0].matches("[a-zA-Z0-9]{5}")) return false;
+
+    //Check for filetype (fixed "TRNLOG" value)
+    //Should ingore case?
+    if(uriTokens[1]==null || !uriTokens[1].equalsIgnoreCase("TRNLOG")) return false;
+
+    //Check for creation timestamp correctness
+    if(uriTokens[2]==null || uriTokens[3]==null) return false;
+
+    SimpleDateFormat daysformat = new SimpleDateFormat("yyyyMMddHHmmss");
+    //Make the format refuse wrong date and time (default behavior is to overflow values in following date)
+    daysformat.setLenient(false);
+
+    try {
+      System.out.println(daysformat.parse(uriTokens[2]+uriTokens[3]));
+    }
+    catch (ParseException e){
+      return false;
+    }
+
+    //Check for progressive value
+    if((uriTokens[4] == null) || !uriTokens[4].matches("[0-9]{3}")){
+        return false;
+    }
+
+    return true;
   }
 
 }
