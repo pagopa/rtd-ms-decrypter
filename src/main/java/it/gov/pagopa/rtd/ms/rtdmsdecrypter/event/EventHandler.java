6159b0103b6f5d8c58c99b05ab43c9a2702fe3f1
tommasolencioni <1Seedeadpeopl>
2022-03-14 16:07:19 +0100

    [CEN-1441] add filters to the event handler for not processing unwanted and faulty events. The filters ara applied to blob state.

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmsdecrypter/event/EventHandler.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsdecrypter/event/EventHandler.java
@@ -1,10 +1,12 @@
 package it.gov.pagopa.rtd.ms.rtdmsdecrypter.event;
 
 import it.gov.pagopa.rtd.ms.rtdmsdecrypter.model.BlobApplicationAware;
+import it.gov.pagopa.rtd.ms.rtdmsdecrypter.model.BlobApplicationAware.Status;
 import it.gov.pagopa.rtd.ms.rtdmsdecrypter.model.EventGridEvent;
 import it.gov.pagopa.rtd.ms.rtdmsdecrypter.service.BlobRestConnectorImpl;
 import it.gov.pagopa.rtd.ms.rtdmsdecrypter.service.DecrypterImpl;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
 import lombok.Getter;
@@ -22,22 +24,25 @@ public class EventHandler {
   /**
    * Constructor.
    *
-   * @param decrypterImpl an instance of a Decrypter
+   * @param decrypterImpl         an instance of a Decrypter
    * @param blobRestConnectorImpl an instance of a blobRestConnector
    * @return a consumer for Event Grid events
    */
   @Bean
   public Consumer<Message<List<EventGridEvent>>> blobStorageConsumer(DecrypterImpl decrypterImpl,
       BlobRestConnectorImpl blobRestConnectorImpl) {
-    // Should be wrapped in try-catch in case of malformed blobs' decryption
+
     return message -> message.getPayload().stream()
         .filter(e -> "Microsoft.Storage.BlobCreated".equals(e.getEventType()))
         .map(EventGridEvent::getSubject)
         .map(BlobApplicationAware::new)
         .filter(b -> !BlobApplicationAware.Application.NOAPP.equals(b.getApp()))
         .map(blobRestConnectorImpl::get)
+        .filter(b -> BlobApplicationAware.Status.DOWNLOADED.equals(b.getStatus()))
         .map(decrypterImpl::decrypt)
+        .filter(b -> BlobApplicationAware.Status.DECRYPTED.equals(b.getStatus()))
         .map(blobRestConnectorImpl::put)
+        .filter(b -> BlobApplicationAware.Status.UPLOADED.equals(b.getStatus()))
         .collect(Collectors.toList());
   }
 
