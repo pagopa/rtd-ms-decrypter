c4ea29a667db9d6897fa1cdcc696f47cf6bfa9ea
TommasoLencioni <tommaso.lencioni@pagopa.it>
2022-03-16 17:53:21 +0100

    [CEN-1452] add tests for the blob cleanup method.

--- src/test/java/it/gov/pagopa/rtd/ms/rtdmsdecrypter/model/BlobApplicationAwareTest.java
+++ src/test/java/it/gov/pagopa/rtd/ms/rtdmsdecrypter/model/BlobApplicationAwareTest.java
@@ -1,12 +1,28 @@
 package it.gov.pagopa.rtd.ms.rtdmsdecrypter.model;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertSame;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
 
+@SpringBootTest
+@ExtendWith(OutputCaptureExtension.class)
 class BlobApplicationAwareTest {
+
+  @Value("${decrypt.resources.base.path}")
+  String resources;
 
   @Test
   void shouldMatchRegexRTD() {
@@ -33,6 +49,59 @@ class BlobApplicationAwareTest {
   void shouldMatchNoApp(String blobUri) {
     BlobApplicationAware myBlob = new BlobApplicationAware(blobUri);
     assertSame(BlobApplicationAware.Application.NOAPP, myBlob.getApp());
+  }
+
+  @Test
+  void shouldCleanLocalFiles() throws IOException {
+    String container = "rtd-transactions-32489876908u74bh781e2db57k098c5ad00000000000";
+    String blobName = "CSTAR.99910.TRNLOG.20220316.164707.001.csv.pgp";
+
+    //Create dummy files to be deleted
+    FileOutputStream encryptedBlob = new FileOutputStream(resources + "/" + blobName);
+    FileOutputStream decryptedBlob = new FileOutputStream(
+        resources + "/" + blobName + ".decrypted");
+
+    BlobApplicationAware fakeBlob = new BlobApplicationAware(
+        "/blobServices/default/containers/" + container + "/blobs/" + blobName);
+    fakeBlob.setTargetDir(resources);
+    fakeBlob.setStatus(BlobApplicationAware.Status.DOWNLOADED);
+
+    assertFalse(fakeBlob.localCleanup());
+  }
+
+  @Test
+  void shouldFailFindingLocalEncryptedFile(CapturedOutput output) throws FileNotFoundException {
+    String container = "rtd-transactions-32489876908u74bh781e2db57k098c5ad00000000000";
+    String blobName = "CSTAR.99910.TRNLOG.20220316.164707.001.csv.pgp";
+
+    //Create dummy file to be deleted
+    FileOutputStream decryptedBlob = new FileOutputStream(
+        resources + "/" + blobName + ".decrypted");
+
+    BlobApplicationAware fakeBlob = new BlobApplicationAware(
+        "/blobServices/default/containers/" + container + "/blobs/" + blobName);
+    fakeBlob.setTargetDir(resources);
+    fakeBlob.setStatus(BlobApplicationAware.Status.DOWNLOADED);
+
+    fakeBlob.localCleanup();
+    assertThat(output.getOut(), containsString("Local blob file missing for deletion:"));
+  }
+
+  @Test
+  void shouldFailFindingLocalDecryptedFile(CapturedOutput output) throws FileNotFoundException {
+    String container = "rtd-transactions-32489876908u74bh781e2db57k098c5ad00000000000";
+    String blobName = "CSTAR.99910.TRNLOG.20220316.164707.001.csv.pgp";
+
+    //Create dummy file to be deleted
+    FileOutputStream encryptedBlob = new FileOutputStream(resources + "/" + blobName);
+
+    BlobApplicationAware fakeBlob = new BlobApplicationAware(
+        "/blobServices/default/containers/" + container + "/blobs/" + blobName);
+    fakeBlob.setTargetDir(resources);
+    fakeBlob.setStatus(BlobApplicationAware.Status.DOWNLOADED);
+
+    fakeBlob.localCleanup();
+    assertThat(output.getOut(), containsString("Local blob file missing for deletion:"));
   }
 
 }
