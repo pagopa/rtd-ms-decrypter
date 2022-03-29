0651805895771a22026336b0ef41b67b8657eb1b
TommasoLencioni <tommaso.lencioni@pagopa.it>
2022-03-29 15:50:22 +0200

    [CEN-1504] Add splitter interface and implementation.

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmsdecrypter/service/BlobSplitterImpl.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsdecrypter/service/BlobSplitterImpl.java
@@ -1,6 +1,7 @@
 package it.gov.pagopa.rtd.ms.rtdmsdecrypter.service;
 
 import it.gov.pagopa.rtd.ms.rtdmsdecrypter.model.BlobApplicationAware;
+import it.gov.pagopa.rtd.ms.rtdmsdecrypter.model.BlobApplicationAware.Status;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.Writer;
@@ -8,10 +9,11 @@ import java.nio.channels.Channels;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.stream.Stream;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.LineIterator;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 /**
@@ -21,15 +23,18 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class BlobSplitterImpl implements BlobSplitter {
 
+  //Max number of lines allowed in one blob chunk.
+  @Value("${splitter.threshold}")
+  private int lineThreshold;
+
   /**
    * Method that spli the content of a blob in chunks of n lines.
    *
    * @param blob to be split.
-   * @param n    max number of lines allowed in one blob chunk.
    * @return a list of blobs that represent the split blob.
    */
-  public List<BlobApplicationAware> split(BlobApplicationAware blob, int n) {
-
+  public Stream<BlobApplicationAware> split(BlobApplicationAware blob) {
+    int n = 100000;
     String blobPath = Path.of(blob.getTargetDir(), blob.getBlob() + ".decrypted").toString();
 
     ArrayList<BlobApplicationAware> blobSplit = new ArrayList<>();
@@ -58,13 +63,17 @@ public class BlobSplitterImpl implements BlobSplitter {
             }
             i++;
           }
+          BlobApplicationAware tmpBlob = new BlobApplicationAware(
+              blob.getBlobUri() + "." + chunkNum);
+          tmpBlob.setStatus(Status.DOWNLOADED);
+          blobSplit.add(tmpBlob);
         }
         chunkNum++;
       }
     } catch (IOException e) {
-      log.error("Missing file:{}", blobPath);
+      log.error("Missing blob file:{}", blobPath);
     }
 
-    return blobSplit;
+    return blobSplit.stream();
   }
 }
