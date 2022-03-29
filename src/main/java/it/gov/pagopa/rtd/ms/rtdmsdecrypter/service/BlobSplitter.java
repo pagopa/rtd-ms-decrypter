e185c2914ef8302d92763c1844f87e6dafdee548
TommasoLencioni <tommaso.lencioni@pagopa.it>
2022-03-29 15:50:22 +0200

    [CEN-1504] Add splitter interface and implementation.

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmsdecrypter/service/BlobSplitter.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsdecrypter/service/BlobSplitter.java
@@ -1,12 +1,12 @@
 package it.gov.pagopa.rtd.ms.rtdmsdecrypter.service;
 
 import it.gov.pagopa.rtd.ms.rtdmsdecrypter.model.BlobApplicationAware;
-import java.util.List;
+import java.util.stream.Stream;
 
 /**
  * Interface that splits the content of a blob storage into arbitrary smaller chunks (blobs).
  */
 public interface BlobSplitter {
 
-  List<BlobApplicationAware> split(BlobApplicationAware blob, int n);
+  Stream<BlobApplicationAware> split(BlobApplicationAware blob);
 }
