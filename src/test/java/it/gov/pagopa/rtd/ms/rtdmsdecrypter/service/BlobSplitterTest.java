b0d6d9b044ef4f118ad1b61fbaf5276cf17aeed2
TommasoLencioni <tommaso.lencioni@pagopa.it>
2022-08-19 12:57:36 +0200

    [TAE-159] refactor and enhance split testing

--- src/test/java/it/gov/pagopa/rtd/ms/rtdmsdecrypter/service/BlobSplitterTest.java
+++ src/test/java/it/gov/pagopa/rtd/ms/rtdmsdecrypter/service/BlobSplitterTest.java
@@ -112,21 +112,85 @@ class BlobSplitterTest {
   }
 
   @Test
-  void shouldSplit(CapturedOutput output) {
+  void shouldSplitRTD(CapturedOutput output) {
 
     blobSplitterImpl.setLineThreshold(1);
 
-    Stream<BlobApplicationAware> chunks = blobSplitterImpl.split(fakeBlob);
+    Stream<BlobApplicationAware> chunks = blobSplitterImpl.split(fakeBlobRTD);
     Iterable<BlobApplicationAware> iterable = chunks::iterator;
     int i = 0;
     for (BlobApplicationAware b : iterable) {
       assertEquals(Status.SPLIT, b.getStatus());
-      assertEquals(blobName + "." + i + ".decrypted", b.getBlob());
+      assertEquals(blobNameRTD + "." + i + ".decrypted", b.getBlob());
       i++;
     }
     assertEquals(3, i);
     assertThat(output.getOut(), containsString("Obtained 3 chunk/s from blob:"));
 
+  }
+
+  @Test
+  void shouldSplitTAE() {
+    //Instantiate a fake blob with clear text content
+    blobSplitterImpl.setLineThreshold(1);
+
+    Stream<BlobApplicationAware> chunks = blobSplitterImpl.split(fakeBlobTAE);
+    Iterable<BlobApplicationAware> iterable = chunks::iterator;
+    int i = 0;
+    for (BlobApplicationAware b : iterable) {
+      assertEquals(Status.SPLIT, b.getStatus());
+      assertEquals(
+          "AGGADE." + fakeBlobTAE.getSenderCode() + "." + fakeBlobTAE.getFileCreationDate() + "."
+              + fakeBlobTAE.getFileCreationTime() + "." + fakeBlobTAE.getFlowNumber() + "." + i,
+          b.getBlob());
+      i++;
+    }
+    assertEquals(4, i);
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {
+      "\n",
+      "00000;00;2022-07-21;2022-07-21;77;249135;978;99999;8894738909374375872;4759769053262163701;00000000003;00000000003;00\n"
+      + "\n"
+      + "00000;00;2022-07-21;2022-07-20;17;249135;978;99999;8894738909374375872;4759769053262163701;00000000005;00000000005;00",
+      ";00;2022-07-21;2022-07-21;77;249135;978;99999;8894738909374375872;4759769053262163701;00000000003;00000000003;00\n",
+      "00000;;2022-07-21;2022-07-21;77;249135;978;00000;8894738909374375872;4759769053262163701;00000000003;00000000003;00\n",
+      "00000;001;2022-07-21;2022-07-21;77;249135;978;00000;8894738909374375872;4759769053262163701;00000000003;00000000003;00\n",
+      "00000;00;;2022-07-21;77;249135;978;00000;8894738909374375872;4759769053262163701;00000000003;00000000003;00\n",
+      "00000;00;2022-07-;2022-07-21;77;249135;978;00000;8894738909374375872;4759769053262163701;00000000003;00000000003;00\n",
+      "00000;00;2022-07-21;;77;249135;978;00000;8894738909374375872;4759769053262163701;00000000003;00000000003;00\n",
+      "00000;00;2022-07-21;2022-07-;77;249135;978;00000;8894738909374375872;4759769053262163701;00000000003;00000000003;00\n",
+      "00000;00;2022-07-21;2022-07-21;-1;249135;978;00000;8894738909374375872;4759769053262163701;00000000003;00000000003;00\n",
+      "00000;00;2022-07-21;2022-07-21;;249135;978;00000;8894738909374375872;4759769053262163701;00000000003;00000000003;00\n",
+      "00000;00;2022-07-21;2022-07-21;-1;249135;978;00000;8894738909374375872;4759769053262163701;00000000003;00000000003;00\n",
+      "00000;00;2022-07-21;2022-07-21;77;;978;00000;8894738909374375872;4759769053262163701;00000000003;00000000003;00\n",
+      "00000;00;2022-07-21;2022-07-21;77;-1;978;00000;8894738909374375872;4759769053262163701;00000000003;00000000003;00\n",
+      "00000;00;2022-07-21;2022-07-21;77;249135;;00000;8894738909374375872;4759769053262163701;00000000003;00000000003;00\n",
+      "00000;00;2022-07-21;2022-07-21;77;249135;EUR;00000;8894738909374375872;4759769053262163701;00000000003;00000000003;00\n",
+      "00000;00;2022-07-21;2022-07-21;77;249135;978;;8894738909374375872;4759769053262163701;00000000003;00000000003;00\n",
+      "00000;00;2022-07-21;2022-07-21;77;249135;978;00000;;4759769053262163701;00000000003;00000000003;00\n",
+      "00000;00;2022-07-21;2022-07-21;77;249135;978;00000;8894738909374375872;;00000000003;00000000003;00\n",
+      "00000;00;2022-07-21;2022-07-21;77;249135;978;00000;8894738909374375872;4759769053262163701;;00000000003;00\n",
+      "00000;00;2022-07-21;2022-07-21;77;249135;978;00000;8894738909374375872;4759769053262163701;00000000003;00000000003;\n",
+      "00000;00;2022-07-21;2022-07-21;77;249135;978;00000;8894738909374375872;4759769053262163701;00000000003;00000000003;0\n",
+      "00000;00;2022-07-21;2022-07-21;77;249135;978;00000;8894738909374375872;4759769053262163701;00000000003;00000000003;000\n"})
+  void shouldFailValidateTAE(String malformedAggregateRecord) throws IOException {
+    Files.write(Path.of(tmpDirectory, blobNameTAEEmpty + ".decrypted"),
+        malformedAggregateRecord.getBytes(), StandardOpenOption.APPEND);
+
+    //Instantiate a fake blob with clear text content
+    blobSplitterImpl.setLineThreshold(1);
+
+    Stream<BlobApplicationAware> chunks = blobSplitterImpl.split(fakeBlobTAEEmpty);
+    Iterable<BlobApplicationAware> iterable = chunks::iterator;
+    int i = 0;
+    for (BlobApplicationAware b : iterable) {
+      assertEquals(Status.DECRYPTED, b.getStatus());
+      assertEquals(fakeBlobTAEEmpty.getBlob(), b.getBlob());
+      i++;
+    }
+    assertEquals(1, i);
   }
 
   //This test, contrary to the previous one, tests the scenario where the file run out of lines
@@ -136,12 +200,12 @@ class BlobSplitterTest {
 
     blobSplitterImpl.setLineThreshold(2);
 
-    Stream<BlobApplicationAware> chunks = blobSplitterImpl.split(fakeBlob);
+    Stream<BlobApplicationAware> chunks = blobSplitterImpl.split(fakeBlobRTD);
     Iterable<BlobApplicationAware> iterable = chunks::iterator;
     int i = 0;
     for (BlobApplicationAware b : iterable) {
       assertEquals(Status.SPLIT, b.getStatus());
-      assertEquals(blobName + "." + i + ".decrypted", b.getBlob());
+      assertEquals(blobNameRTD + "." + i + ".decrypted", b.getBlob());
       i++;
     }
     assertEquals(2, i);
@@ -153,11 +217,11 @@ class BlobSplitterTest {
   void shouldNotSplitMissingFile(CapturedOutput output) {
 
     //Set the wrong directory for locating the decrypted fake blob
-    fakeBlob.setTargetDir("pippo");
-    fakeBlob.setStatus(BlobApplicationAware.Status.DOWNLOADED);
+    fakeBlobRTD.setTargetDir("pippo");
+    fakeBlobRTD.setStatus(BlobApplicationAware.Status.DOWNLOADED);
     blobSplitterImpl.setLineThreshold(1);
 
-    blobSplitterImpl.split(fakeBlob);
+    blobSplitterImpl.split(fakeBlobRTD);
     assertThat(output.getOut(), containsString("Missing blob file:"));
 
   }
