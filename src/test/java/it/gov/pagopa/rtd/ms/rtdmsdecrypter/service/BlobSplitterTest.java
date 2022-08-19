b188a026399b4869adc457024ecbbaacdf582e22
TommasoLencioni <tommaso.lencioni@pagopa.it>
2022-08-19 12:56:53 +0200

    [TAE-159] add ADE test blob creation in setup step

--- src/test/java/it/gov/pagopa/rtd/ms/rtdmsdecrypter/service/BlobSplitterTest.java
+++ src/test/java/it/gov/pagopa/rtd/ms/rtdmsdecrypter/service/BlobSplitterTest.java
@@ -5,18 +5,22 @@ import static org.hamcrest.Matchers.containsString;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import it.gov.pagopa.rtd.ms.rtdmsdecrypter.model.BlobApplicationAware;
+import it.gov.pagopa.rtd.ms.rtdmsdecrypter.model.BlobApplicationAware.Application;
 import it.gov.pagopa.rtd.ms.rtdmsdecrypter.model.BlobApplicationAware.Status;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.stream.Stream;
 import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -41,27 +45,65 @@ class BlobSplitterTest {
   String tmpDirectory;
 
   String container = "rtd-transactions-32489876908u74bh781e2db57k098c5ad00000000000";
-  String blobName = "CSTAR.99910.TRNLOG.20220228.103107.001.csv.pgp";
-  BlobApplicationAware fakeBlob;
+  String blobNameRTD = "CSTAR.99999.TRNLOG.20220419.121045.001.csv";
+
+  String blobNameTAE = "ADE.99999.TRNLOG.20220721.095718.001.csv";
+
+  String blobNameTAEEmpty = "ADE.00000.TRNLOG.20220721.095718.001.csv";
+
+  BlobApplicationAware fakeBlobRTD;
+
+  BlobApplicationAware fakeBlobTAE;
+
+  BlobApplicationAware fakeBlobTAEEmpty;
 
   @BeforeEach
   void setUp() throws IOException {
 
-    String transactions = "cleartext.csv";
-
-    //Create the decrypted file
-    File decryptedFile = Path.of(tmpDirectory, blobName + ".decrypted").toFile();
+    //Create the decrypted file for RTD
+    File decryptedFile = Path.of(tmpDirectory, blobNameRTD).toFile();
     decryptedFile.getParentFile().mkdirs();
     decryptedFile.createNewFile();
     FileOutputStream decryptedStream = new FileOutputStream(
-        Path.of(tmpDirectory, blobName + ".decrypted").toString());
-    Files.copy(Path.of(resources, transactions), decryptedStream);
+        Path.of(tmpDirectory, blobNameRTD + ".decrypted").toString());
+    Files.copy(Path.of(resources, blobNameRTD), decryptedStream);
 
-    //Instantiate a fake blob with clear text content
-    fakeBlob = new BlobApplicationAware(
-        "/blobServices/default/containers/" + container + "/blobs/" + blobName);
-    fakeBlob.setTargetDir(tmpDirectory);
-    fakeBlob.setStatus(Status.DECRYPTED);
+    //Instantiate a fake RTD blob with clear text content
+    fakeBlobRTD = new BlobApplicationAware(
+        "/blobServices/default/containers/" + container + "/blobs/" + blobNameRTD);
+    fakeBlobRTD.setTargetDir(tmpDirectory);
+    fakeBlobRTD.setStatus(Status.DECRYPTED);
+    fakeBlobRTD.setApp(Application.RTD);
+
+    //Create the decrypted file for TAE
+    File decryptedFileAggregates = Path.of(tmpDirectory, blobNameTAE).toFile();
+    decryptedFileAggregates.getParentFile().mkdirs();
+    decryptedFileAggregates.createNewFile();
+    FileOutputStream decryptedStreamAggregates = new FileOutputStream(
+        Path.of(tmpDirectory, blobNameTAE + ".decrypted").toString());
+    Files.copy(Path.of(resources, blobNameTAE), decryptedStreamAggregates);
+
+    //Instantiate a fake TAE blob with clear text content
+    fakeBlobTAE = new BlobApplicationAware(
+        "/blobServices/default/containers/" + container + "/blobs/" + blobNameTAE);
+    fakeBlobTAE.setTargetDir(tmpDirectory);
+    fakeBlobTAE.setStatus(Status.DECRYPTED);
+    fakeBlobTAE.setApp(Application.ADE);
+
+    //Create the decrypted empty file for TAE
+    File decryptedFileAggregatesEmpty = Path.of(tmpDirectory, blobNameTAEEmpty).toFile();
+    decryptedFileAggregatesEmpty.getParentFile().mkdirs();
+    decryptedFileAggregatesEmpty.createNewFile();
+    FileOutputStream decryptedStreamAggregatesEmpty = new FileOutputStream(
+        Path.of(tmpDirectory, blobNameTAEEmpty + ".decrypted").toString());
+    Files.copy(Path.of(resources, blobNameTAEEmpty), decryptedStreamAggregatesEmpty);
+
+    //Instantiate a fake TAE blob with clear text content
+    fakeBlobTAEEmpty = new BlobApplicationAware(
+        "/blobServices/default/containers/" + container + "/blobs/" + blobNameTAEEmpty);
+    fakeBlobTAEEmpty.setTargetDir(tmpDirectory);
+    fakeBlobTAEEmpty.setStatus(Status.DECRYPTED);
+    fakeBlobTAEEmpty.setApp(Application.ADE);
   }
 
   @AfterEach
