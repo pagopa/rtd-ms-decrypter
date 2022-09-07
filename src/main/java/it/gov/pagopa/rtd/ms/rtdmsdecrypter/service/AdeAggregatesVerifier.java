8b7f2a03658f620dda7aff69118133841fd3f20b
TommasoLencioni <tommaso.lencioni@pagopa.it>
2022-09-07 10:17:12 +0200

    [RTD-466] add TAE aggregates verifier

---
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsdecrypter/service/AdeAggregatesVerifier.java
@@ -0,0 +1,60 @@
+package it.gov.pagopa.rtd.ms.rtdmsdecrypter.service;
+
+import com.opencsv.bean.BeanVerifier;
+import com.opencsv.exceptions.CsvConstraintViolationException;
+import it.gov.pagopa.rtd.ms.rtdmsdecrypter.model.AdeTransactionsAggregate;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Set;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+public class AdeAggregatesVerifier implements BeanVerifier {
+
+  private static final ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+
+  private static final Validator validator = factory.getValidator();
+
+  @Override
+  public boolean verifyBean(Object o) throws CsvConstraintViolationException {
+    AdeTransactionsAggregate adeTransactionsAggregate = (AdeTransactionsAggregate) o;
+    Set<ConstraintViolation<AdeTransactionsAggregate>> violations = validator.validate(
+        adeTransactionsAggregate);
+
+    if (!violations.isEmpty()) {
+      StringBuilder malformedFields = new StringBuilder();
+      for (ConstraintViolation<AdeTransactionsAggregate> violation : violations) {
+        malformedFields.append("(").append(violation.getPropertyPath().toString()).append(": ");
+        malformedFields.append(violation.getMessage()).append(") ");
+      }
+
+      throw new CsvConstraintViolationException("Malformed fields extracted: "
+          + malformedFields);
+    }
+
+    if (!validateDate(adeTransactionsAggregate.getTransmissionDate())) {
+      throw new CsvConstraintViolationException(
+          "Invalid transmission date " + adeTransactionsAggregate.getTransmissionDate());
+
+    }
+    if (!validateDate(adeTransactionsAggregate.getAccountingDate())) {
+      throw new CsvConstraintViolationException(
+          "Invalid accounting date " + adeTransactionsAggregate.getAccountingDate());
+    }
+
+    return true;
+  }
+
+  private boolean validateDate(String date) {
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    try {
+      LocalDate.parse(date, formatter);
+      return true;
+    } catch (DateTimeParseException e) {
+      return false;
+    }
+  }
+}
