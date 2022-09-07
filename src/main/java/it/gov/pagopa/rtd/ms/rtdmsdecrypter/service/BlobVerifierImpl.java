6dc2467cf6a47ee826bd9f0a2f3ee6a528e72a6f
TommasoLencioni <tommaso.lencioni@pagopa.it>
2022-09-07 17:04:31 +0200

    [RTD-466] serialize verified records without checksum

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmsdecrypter/service/BlobVerifierImpl.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsdecrypter/service/BlobVerifierImpl.java
@@ -5,14 +5,18 @@ import static it.gov.pagopa.rtd.ms.rtdmsdecrypter.model.BlobApplicationAware.Sta
 import com.opencsv.bean.BeanVerifier;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+import com.opencsv.bean.StatefulBeanToCsv;
+import com.opencsv.bean.StatefulBeanToCsvBuilder;
 import com.opencsv.exceptions.CsvException;
 import it.gov.pagopa.rtd.ms.rtdmsdecrypter.model.AdeTransactionsAggregate;
 import it.gov.pagopa.rtd.ms.rtdmsdecrypter.model.BlobApplicationAware;
 import it.gov.pagopa.rtd.ms.rtdmsdecrypter.model.BlobApplicationAware.Application;
 import it.gov.pagopa.rtd.ms.rtdmsdecrypter.model.DecryptedRecord;
 import it.gov.pagopa.rtd.ms.rtdmsdecrypter.model.RtdTransaction;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.nio.file.Path;
 import java.util.List;
 import lombok.Setter;
@@ -35,6 +39,14 @@ public class BlobVerifierImpl implements BlobVerifier {
   public BlobApplicationAware verify(BlobApplicationAware blob) {
     log.info("Start evaluating blob {} from {}", blob.getBlob(), blob.getContainer());
 
+    FileReader fileReader;
+
+    try {
+      fileReader = new FileReader(Path.of(blob.getTargetDir(), blob.getBlob()).toFile());
+    } catch (FileNotFoundException e) {
+      return blob;
+    }
+
     BeanVerifier<? extends DecryptedRecord> verifier = new RtdTransactionsVerifier();
     Class<? extends DecryptedRecord> beanClass = RtdTransaction.class;
 
@@ -43,20 +55,17 @@ public class BlobVerifierImpl implements BlobVerifier {
       beanClass = AdeTransactionsAggregate.class;
     }
 
-    try {
-      CsvToBean<DecryptedRecord> b = new CsvToBeanBuilder<DecryptedRecord>(new FileReader(
-          Path.of(blob.getTargetDir(), blob.getBlob()).toFile()))
-          .withType(beanClass)
-          .withSeparator(';')
-          .withVerifier((BeanVerifier<DecryptedRecord>) verifier)
-          // skip the checksum line
-          .withSkipLines(1)
-          .withThrowExceptions(false)
-          .build();
+    CsvToBean<DecryptedRecord> b = new CsvToBeanBuilder<DecryptedRecord>(fileReader)
+        .withType(beanClass)
+        .withSeparator(';')
+        .withVerifier((BeanVerifier<DecryptedRecord>) verifier)
+        // skip the checksum line
+        .withSkipLines(1)
+        .withThrowExceptions(false)
+        .build();
 
-      // The parsing is necessary to trigger the verification of the beans
-      List<DecryptedRecord> deserialized = b.parse();
-      List<CsvException> exceptions = b.getCapturedExceptions();
+    List<DecryptedRecord> deserialized = b.parse();
+    List<CsvException> exceptions = b.getCapturedExceptions();
 
     if (deserialized.isEmpty()) {
       if (!exceptions.isEmpty()) {
@@ -78,12 +87,23 @@ public class BlobVerifierImpl implements BlobVerifier {
       return blob;
     }
 
-      log.info("Successful validation of blob:{}", blob.getBlob());
-      blob.setStatus(VERIFIED);
-      return blob;
+    // Serialize the valid records to a new file without the checksum header
+    try (BufferedWriter writer = new BufferedWriter(new FileWriter(
+        Path.of(blob.getTargetDir(), blob.getBlob()).toFile()))) {
 
-    } catch (FileNotFoundException e) {
-      return blob;
+      StatefulBeanToCsv<DecryptedRecord> beanToCsv = new StatefulBeanToCsvBuilder<DecryptedRecord>(
+          writer)
+          .withSeparator(';')
+          .withApplyQuotesToAll(false)
+          .build();
+      beanToCsv.write(deserialized);
+
+    } catch (Exception e) {
+      log.error("Error writing to file", e);
     }
+
+    log.info("Successful validation of blob:{}", blob.getBlob());
+    blob.setStatus(VERIFIED);
+    return blob;
   }
 }
