ac3b51418a3abac4e077a878d8412244ee665a81
TommasoLencioni <tommaso.lencioni@pagopa.it>
2022-09-07 10:21:17 +0200

    [RTD-466] add RTD transaction verifier

---
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsdecrypter/service/RtdTransactionsVerifier.java
@@ -0,0 +1,41 @@
+package it.gov.pagopa.rtd.ms.rtdmsdecrypter.service;
+
+import com.opencsv.bean.BeanVerifier;
+import com.opencsv.exceptions.CsvConstraintViolationException;
+import it.gov.pagopa.rtd.ms.rtdmsdecrypter.model.RtdTransaction;
+import java.util.Set;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+/**
+ * Implementation of {@link BeanVerifier}, used to verify the validity of the {@link RtdTransaction}
+ * records extracted from the input file.
+ */
+public class RtdTransactionsVerifier implements BeanVerifier {
+
+  private static final ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+
+  private static final Validator validator = factory.getValidator();
+
+  @Override
+  public boolean verifyBean(Object o) throws CsvConstraintViolationException {
+    RtdTransaction rtdTransactions = (RtdTransaction) o;
+    Set<ConstraintViolation<RtdTransaction>> violations = validator.validate(
+        rtdTransactions);
+
+    if (!violations.isEmpty()) {
+      StringBuilder malformedFields = new StringBuilder();
+      for (ConstraintViolation<RtdTransaction> violation : violations) {
+        malformedFields.append("(").append(violation.getPropertyPath().toString()).append(": ");
+        malformedFields.append(violation.getMessage()).append(") ");
+      }
+
+      throw new CsvConstraintViolationException("Malformed fields extracted: "
+          + malformedFields);
+    }
+
+    return true;
+  }
+}
