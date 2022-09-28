32b5fe013780607bba4f8049bb8c3a53e81dccad
TommasoLencioni <tommaso.lencioni@pagopa.it>
2022-09-28 15:33:54 +0200

    [RTD-599] validate in stream instead of deserialize all the file in memory

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmsdecrypter/service/BlobVerifierImpl.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsdecrypter/service/BlobVerifierImpl.java
@@ -5,24 +5,15 @@ import static it.gov.pagopa.rtd.ms.rtdmsdecrypter.model.BlobApplicationAware.Sta
 import com.opencsv.bean.BeanVerifier;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
-import com.opencsv.bean.StatefulBeanToCsv;
-import com.opencsv.bean.StatefulBeanToCsvBuilder;
-import com.opencsv.exceptions.CsvDataTypeMismatchException;
 import com.opencsv.exceptions.CsvException;
-import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
 import it.gov.pagopa.rtd.ms.rtdmsdecrypter.config.VerifierFactory;
-import it.gov.pagopa.rtd.ms.rtdmsdecrypter.model.AdeTransactionsAggregate;
 import it.gov.pagopa.rtd.ms.rtdmsdecrypter.model.BlobApplicationAware;
-import it.gov.pagopa.rtd.ms.rtdmsdecrypter.model.BlobApplicationAware.Application;
 import it.gov.pagopa.rtd.ms.rtdmsdecrypter.model.DecryptedRecord;
-import it.gov.pagopa.rtd.ms.rtdmsdecrypter.model.RtdTransaction;
-import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.stream.Stream;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -78,7 +69,9 @@ public class BlobVerifierImpl implements BlobVerifier {
 
     CsvToBean<DecryptedRecord> csvToBean = builder.build();
 
-    List<DecryptedRecord> deserialized = csvToBean.parse();
+    Stream<DecryptedRecord> deserialized = csvToBean.stream();
+
+    long numberOfDeserializeRecords = deserialized.count();
     List<CsvException> violations = csvToBean.getCapturedExceptions();
 
     if (!violations.isEmpty()) {
@@ -86,14 +79,8 @@ public class BlobVerifierImpl implements BlobVerifier {
         log.error("Validation error at line " + e.getLineNumber() + " : " + e.getMessage());
       }
       isValid = false;
-    } else if (deserialized.isEmpty()) {
+    } else if (numberOfDeserializeRecords == 0) {
       log.error("No records found in file {}", blob.getBlob());
-      isValid = false;
-    }
-
-    try {
-      serializeValidRecords(deserialized, blob.getTargetDir(), blob.getBlob());
-    } catch (Exception e) {
       isValid = false;
     }
 
@@ -103,30 +90,14 @@ public class BlobVerifierImpl implements BlobVerifier {
       blob.localCleanup();
     }
 
-    logVerificationInformation(blob.getBlob(), deserialized.size(), violations.size());
+    logVerificationInformation(blob.getOriginalBlobName(), numberOfDeserializeRecords,
+        violations.size());
     return blob;
   }
 
-  private void logVerificationInformation(String blobName, Integer deserializedSize,
+  private void logVerificationInformation(String blobName, Long deserializedSize,
       Integer violations) {
     log.info("END Verifying {} records: {} valid , {} malformed", blobName,
         deserializedSize, violations);
-  }
-
-  private void serializeValidRecords(List<DecryptedRecord> deserialized, String targetDir,
-      String blobName)
-      throws IOException, CsvRequiredFieldEmptyException, CsvDataTypeMismatchException {
-    try (BufferedWriter writer = new BufferedWriter(new FileWriter(
-        Path.of(targetDir, blobName).toFile()))) {
-      StatefulBeanToCsv<DecryptedRecord> beanToCsv = new StatefulBeanToCsvBuilder<DecryptedRecord>(
-          writer)
-          .withSeparator(';')
-          .withApplyQuotesToAll(false)
-          .build();
-      beanToCsv.write(deserialized);
-    } catch (Exception e) {
-      log.error("Error writing to file {}", blobName);
-      throw e;
-    }
   }
 }
