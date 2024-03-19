f2e8fdcc6a9998a05dfa8de23cc009b2e5a39877
TommasoLencioni <tommaso.lencioni@pagopa.it>
2024-03-19 13:45:35 +0100

    feat: [RTD-2501] extend card bin to 8 integers

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmsdecrypter/model/ContractMethodAttributes.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsdecrypter/model/ContractMethodAttributes.java
@@ -40,6 +40,6 @@ public class ContractMethodAttributes {
 
   @JsonProperty("card_bin")
   @NotNull
-  @Pattern(regexp = "\\d{6}")
+  @Pattern(regexp = "\\d{6}|\\d{8}")
   private String cardBin;
 }
