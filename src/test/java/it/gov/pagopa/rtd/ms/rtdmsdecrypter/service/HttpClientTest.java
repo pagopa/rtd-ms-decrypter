86a6ad6ea905b2a432e5adce826d634f97686e81
TommasoLencioni <tommaso.lencioni@pagopa.it>
2024-05-31 16:09:10 +0200

    chore: [RTD-2551] use OTEL instrumented client as HTTP client

--- src/test/java/it/gov/pagopa/rtd/ms/rtdmsdecrypter/service/HttpClientTest.java
+++ src/test/java/it/gov/pagopa/rtd/ms/rtdmsdecrypter/service/HttpClientTest.java
@@ -1,33 +1,41 @@
 package it.gov.pagopa.rtd.ms.rtdmsdecrypter.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-import it.gov.pagopa.rtd.ms.rtdmsdecrypter.configuration.ThreadSafeHttpClient;
 import java.io.IOException;
+import org.apache.hc.client5.http.classic.HttpClient;
 import org.apache.hc.client5.http.classic.methods.HttpGet;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.core5.http.HttpResponse;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@ContextConfiguration(classes = {ThreadSafeHttpClient.class})
+@ContextConfiguration(classes = {Mockito.class})
 @TestPropertySource(value = {"classpath:application-nokafka.yml"}, inheritProperties = false)
 class HttpClientTest {
 
-  @Autowired
-  CloseableHttpClient myClient;
+  @Mock
+  HttpClient client;
 
   @Test
   void shouldNotBeClosed() throws IOException {
-    String uri = "https://eu.httpbin.org/get";
-    final HttpGet getBlob = new HttpGet(uri);
-    var statusCode = myClient.execute(getBlob, HttpResponse::getCode);
 
-    assertEquals(200, statusCode);
+    HttpResponse httpResponse = mock(HttpResponse.class);
+    when(httpResponse.getCode()).thenReturn(200);
+    when(client.execute(any(HttpGet.class))).thenReturn(httpResponse);
+    String uri = "https://eu.httpbin.org/get";
+
+    final HttpGet getBlob = new HttpGet(uri);
+    HttpResponse res = client.execute(getBlob);
+
+    assertEquals(200, res.getCode());
   }
 
 }
