d2284656d9a03f6aafa2ccbbf66464334a09ddf2
Andrea Morabito <andrea.morabito@pagopa.it>
2024-06-14 18:17:13 +0200

    config graalvm to initialize correctly the provider bouncy castle

---
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsdecrypter/config/BouncyCastleFeature.java
@@ -0,0 +1,17 @@
+package it.gov.pagopa.rtd.ms.rtdmsdecrypter.config;
+
+
+import java.security.Security;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.graalvm.nativeimage.hosted.Feature;
+import org.graalvm.nativeimage.hosted.RuntimeClassInitialization;
+
+public class BouncyCastleFeature implements Feature {
+
+  @Override
+  public void afterRegistration(AfterRegistrationAccess access) {
+    RuntimeClassInitialization.initializeAtBuildTime("org.bouncycastle");
+    Security.addProvider(new BouncyCastleProvider());
+  }
+
+}
